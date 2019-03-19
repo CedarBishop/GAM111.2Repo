a54a7c06a84619ca6c09f7a04f2376091162460a
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-20 01:03:35 +1100

    Created Turn Based Script

---
+++ GAM111.2/Assets/Scripts/TurnBasedSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class TurnBasedSystem : MonoBehaviour
+{
+    public enum CurrentAnimalTurn { Player, Enemy, None};
+    CurrentAnimalTurn currentAnimalTurn;
+    [HideInInspector] public static bool playersTurn;
+    [HideInInspector] public static bool enemiesTurn;
+
+    void Start()
+    {
+        currentAnimalTurn = CurrentAnimalTurn.Player;
+    }
+
+
+    void Update()
+    {
+        switch (currentAnimalTurn)
+        {
+            case CurrentAnimalTurn.Player:
+                playersTurn = true;
+                enemiesTurn = false;
+                break;
+            case CurrentAnimalTurn.Enemy:
+                playersTurn = false;
+                enemiesTurn = true;
+                break;
+            case CurrentAnimalTurn.None:
+                playersTurn = false;
+                enemiesTurn = false;
+                break;
+        }
+    }
+
+    public void CycleNextTurn ()
+    {
+        switch (currentAnimalTurn)
+        {
+            case CurrentAnimalTurn.Player:
+                currentAnimalTurn = CurrentAnimalTurn.Enemy;
+                break;
+            case CurrentAnimalTurn.Enemy:
+                currentAnimalTurn = CurrentAnimalTurn.None;
+                break;
+            case CurrentAnimalTurn.None:
+                currentAnimalTurn = CurrentAnimalTurn.Player;
+                break;
+        }
+    }
+}
