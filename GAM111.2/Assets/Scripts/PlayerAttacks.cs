a31f27c175a26e844c1e91b5efbd39f42faad74c
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-19 13:20:00 +1100

    Added Player Attack Buttons and Script template

---
+++ GAM111.2/Assets/Scripts/PlayerAttacks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacks : MonoBehaviour
+{
+    
+    public void FirePunch ()
+    {
+
+    }
+
+    public void SweepKick ()
+    {
+
+    }
+
+    public void IceTornado ()
+    {
+
+    }
+
+    public void Heal ()
+    {
+
+    }
+
+
+}
