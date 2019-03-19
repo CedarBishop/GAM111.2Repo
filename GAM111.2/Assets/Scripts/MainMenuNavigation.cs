b3ab1b81feb04e465dd60e375481a135d072013a
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-19 15:44:44 +1100

    Added Pause Menu, Mouseless pause menu controls and player attack method definitions

---
+++ GAM111.2/Assets/Scripts/MainMenuNavigation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuNavigation : MonoBehaviour
+{
+    public string GameSceneName;
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
--- GAM111.2/Assets/Scripts/PlayerAttacks.cs
+++ GAM111.2/Assets/Scripts/PlayerAttacks.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttacks : MonoBehaviour
 {
-    
+    public Canvas buttonCanvas;
+    public TMPro.TextMeshProUGUI playerCurrentAttackText;
+    void Start ()
+    {
+        playerCurrentAttackText.text = "";
+    }
     public void FirePunch ()
     {
-
+        buttonCanvas.gameObject.SetActive(false);
+        playerCurrentAttackText.text = "Player used Fire Punch!";
     }
 
     public void SweepKick ()
     {
-
+        buttonCanvas.gameObject.SetActive(false);
+        playerCurrentAttackText.text = "Player used Sweep Kick!";
     }
 
     public void IceTornado ()
     {
-
+        buttonCanvas.gameObject.SetActive(false);
+        playerCurrentAttackText.text = "Player used Ice Tornado!";
     }
 
     public void Heal ()
     {
-
+        buttonCanvas.gameObject.SetActive(false);
+        playerCurrentAttackText.text = "Player used Heal!";
     }
 
 
