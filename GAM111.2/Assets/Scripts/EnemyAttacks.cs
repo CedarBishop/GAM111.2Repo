69e74c2e677f5373caa6c76bc6251efcc1064d3b
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-02 00:19:56 +1100

    Fixed null reference for enemy attack text

--- GAM111.2/Assets/Scripts/EnemyAttacks.cs
+++ GAM111.2/Assets/Scripts/EnemyAttacks.cs
@@ -9,23 +9,22 @@ public class EnemyAttacks : MonoBehaviour
     TMPro.TextMeshProUGUI enemyAttackText;
     public float damageEnemyDeals = 25;
     // Start is called before the first frame update
-    void Start()
-    {
-        battleLogic = GameObject.Find("Scene Manager").GetComponent<BattleLogic>();
-        enemyAttackText = GameObject.Find("Enemy Attack Text").GetComponent<TMPro.TextMeshProUGUI>();
-    }
+    
     public void AttackPlayer ()
     {
+        enemyAttackText = GameObject.Find("Enemy Attack Text").GetComponent<TMPro.TextMeshProUGUI>();
         enemyAttackText.text = enemyAttackName;
     }
 
     public void EnemyAttackDamageStep ()
     {
+        battleLogic = GameObject.Find("Scene Manager").GetComponent<BattleLogic>();
         battleLogic.PlayerTakeDamage(damageEnemyDeals);
 
     }
     public void ClearEnemyAttackText ()
     {
+        enemyAttackText = GameObject.Find("Enemy Attack Text").GetComponent<TMPro.TextMeshProUGUI>();
         enemyAttackText.text = "";
     }
 }
